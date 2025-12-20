3220b738de457d1a648351c77e512e66b946b724
Rodrigo Garcia <rgarcia.garcia@gmail.com>
2025-12-20 23:09:40 +0100

    solution to part 2

--- day1/src/day1.adb
+++ day1/src/day1.adb
@@ -3,36 +3,47 @@ with Ada.Text_IO; use Ada.Text_IO;
 procedure Day1 is
    type Dial_Position is mod 100;
    Dial : Dial_Position := 50;
-   Password : Natural := 0;
+   Password_1 : Natural := 0;
+   Password_2 : Natural := 0;
 
    procedure Turn_Dial (Rotation : String) is
-      Direction : Character := Rotation (Rotation'First);
-      Quantity : Dial_Position := Dial_Position (Positive'Value
-         (Rotation (Rotation'First + 1 .. Rotation'Last)) mod 100);
+      Direction : constant Character := Rotation (Rotation'First);
+      Quantity : constant Positive := Positive'Value
+         (Rotation (Rotation'First + 1 .. Rotation'Last));
+      Full_Turns : constant Natural := Quantity / 100;
+      Effective : constant Dial_Position := Dial_Position (Quantity mod 100);
    begin
+      Password_2 := Password_2 + Full_Turns;
       if Direction = 'L' then
-         Dial := Dial - Quantity;
+         if (Dial /= 0) and then (Dial <= Effective) then
+            Password_2 := Password_2 + 1;
+         end if;
+         Dial := Dial - Effective;
       elsif Direction = 'R' then
-         Dial := Dial + Quantity;
+         if (Dial /= 0) and then ((99 - Dial) < Effective) then
+            Password_2 := Password_2 + 1;
+         end if;
+         Dial := Dial + Effective;
       end if;
    end Turn_Dial;
 
    Document : File_Type;
    Doc_Name : constant String := "input.txt";
 begin
-   Open (Document, In_File, Doc_name);
+   Open (Document, In_File, Doc_Name);
    while not End_Of_File (Document) loop
       declare
          Doc_Line : constant String := Get_Line (Document);
       begin
-         Put_Line ("Read rotation " & Doc_Line);
          Turn_Dial (Doc_Line);
-         Put_Line ("Current dial position: " & Dial_Position'Image (Dial));
+         Put_Line ("Dial position after rotation " &
+            Doc_Line & ": " & Dial_Position'Image (Dial));
          if Dial = 0 then
-            Password := Password + 1;
+            Password_1 := Password_1 + 1;
          end if;
       end;
    end loop;
    Close (Document);
-   Put_Line ("The password is " & Natural'Image (Password));
+   Put_Line ("The password for part 1 is " & Natural'Image (Password_1));
+   Put_Line ("The password for part 2 is " & Natural'Image (Password_2));
 end Day1;
