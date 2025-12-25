78da68c9df1e649b3b89fc4433e4d01e2acb4025
Rodrigo Garcia <rgarcia.garcia@gmail.com>
2025-12-25 11:13:59 +0100

    solution day 4 part 1

--- day4/src/day4.adb
+++ day4/src/day4.adb
@@ -25,10 +25,69 @@ procedure Day4 is
       end return;
    end Get_Grid_Size;
 
-   Grid_Size : constant Dimension := Get_Grid_Size ("test.txt");
+   Grid_File_Name : constant String := "test.txt";
+   Grid_Size : constant Dimension := Get_Grid_Size (Grid_File_Name);
+   subtype Row is Positive range 1 .. Grid_Size.Y;
+   subtype Column is Positive range 1 .. Grid_Size.X;
+
+   Roll_Grid : array (Row'Range) of String (Column'Range);
+   Roll_Here : array (Row'Range, Column'Range) of Boolean :=
+      (others => (others => False));
+   Adjacents : array (Row'Range, Column'Range) of Natural :=
+      (others => (others => 0));
 begin
-   Put_Line ("Size of Grid is : " & 
-             Positive'Image (Grid_Size.X) &
-             " - " &
-             Positive'Image (Grid_Size.Y));
+   declare
+      Grid_File : File_Type;
+   begin
+      Open (Grid_File, In_File, Grid_File_Name);
+      for Grid_Line of Roll_Grid loop
+         Grid_Line := Get_Line (Grid_File);
+      end loop;
+      Close (Grid_File);
+   end;
+
+   for M in Row'Range loop
+      for N in Column'Range loop
+         if Roll_Grid (M)(N) = '@' then
+            Roll_Here (M, N) := True;
+            if M + 1 in Row'Range then
+               Adjacents (M + 1, N) := Adjacents (M + 1, N) + 1;
+               if N + 1 in Column'Range then
+                  Adjacents (M + 1, N + 1) := Adjacents (M + 1, N + 1) + 1;
+               end if;
+               if N - 1 in Column'Range then
+                  Adjacents (M + 1, N - 1) := Adjacents (M + 1, N - 1) + 1;
+               end if;
+            end if;
+            if M - 1 in Row'Range then
+               Adjacents (M - 1, N) := Adjacents (M - 1, N) + 1;
+               if N + 1 in Column'Range then
+                  Adjacents (M - 1, N + 1) := Adjacents (M - 1, N + 1) + 1;
+               end if;
+               if N - 1 in Column'Range then
+                  Adjacents (M - 1, N - 1) := Adjacents (M - 1, N - 1) + 1;
+               end if;
+            end if;
+            if N + 1 in Column'Range then
+               Adjacents (M, N + 1) := Adjacents (M, N + 1) + 1;
+            end if;
+            if N - 1 in Column'Range then
+               Adjacents (M, N - 1) := Adjacents (M, N - 1) + 1;
+            end if;
+         end if;
+      end loop;
+   end loop;
+
+   declare
+      Accessible_Rolls : Natural := 0;
+   begin
+      for M in Row'Range loop
+         for N in Column'Range loop
+            if Roll_Here (M, N) and then Adjacents (M, N) < 4 then
+               Accessible_Rolls := Accessible_Rolls + 1;
+            end if;
+         end loop;
+      end loop;
+      Put_Line ("Total accessible rolls:" & Accessible_Rolls'Image);
+   end;
 end Day4;
