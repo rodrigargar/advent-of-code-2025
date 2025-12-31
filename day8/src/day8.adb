66d4f95c84c6712cf1fb928b03b877f48a0d3706
Rodrigo Garcia <rgarcia.garcia@gmail.com>
2025-12-31 13:22:12 +0100

    starting day 8

---
+++ day8/src/day8.adb
@@ -0,0 +1,118 @@
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Strings;
+with Ada.Strings.Maps;
+with Ada.Strings.Fixed;
+with Ada.Containers.Generic_Array_Sort;
+with Ada.Containers.Generic_Constrained_Array_Sort;
+with Ada.Containers.Bounded_Ordered_Sets;
+
+procedure Day8 is
+   Boxes_File_Name : constant String := "test.txt";
+   Connections : constant Positive := 10;
+   Connected_Boxes : constant Positive := 2 * Connections;
+
+   type Position is
+      record
+         X, Y, Z : Long_Integer := 0;
+      end record;
+
+   type Pair_Distance is
+      record
+         A, B : Positive;
+         Distance : Long_Integer;
+      end record;
+
+   function "<" (Left, Right : Pair_Distance) return Boolean is
+   begin
+      return Left.Distance < Right.Distance;
+   end "<";
+
+   function Compute_Distance (A, B : Position) return Long_Integer is
+   begin
+      return (A.X - B.X) ** 2 + (A.Y - B.Y) ** 2 + (A.Z - B.Z) ** 2;
+   end Compute_Distance;
+
+   type Junction_Boxes is array (Positive range <>) of Position;
+   type Box_Distances is array (Positive range <>) of Pair_Distance;
+
+   function Get_Boxes (File_Name : String) return Junction_Boxes is
+      package Str renames Ada.Strings;
+
+      Boxes_File : File_Type;
+      N_Boxes : Natural := 0;
+      Comma : constant Str.Maps.Character_Set := Str.Maps.To_Set (',');
+   begin
+      Open (Boxes_File, In_File, File_Name);
+      while not End_Of_File (Boxes_File) loop
+         Skip_Line (Boxes_File);
+         N_Boxes := N_Boxes + 1;
+      end loop;
+      Reset (Boxes_File);
+      return Boxes : Junction_Boxes (1 .. N_Boxes) do
+         for Box of Boxes loop
+            declare
+               Line : constant String := Get_Line (Boxes_File);
+               First_Comma, Second_Comma : Natural;
+            begin
+               First_Comma := Str.Fixed.Index (Line, Comma);
+               Second_Comma := Str.Fixed.Index (Line, Comma, First_Comma + 1);
+               Box.X := Long_Integer'Value
+                  (Line (Line'First .. First_Comma - 1));
+               Box.Y := Long_Integer'Value
+                  (Line (First_Comma + 1 .. Second_Comma - 1));
+               Box.Z := Long_Integer'Value
+                  (Line (Second_Comma + 1 .. Line'Last));
+            end;
+         end loop;
+      end return;
+   end Get_Boxes;
+
+   Boxes : constant Junction_Boxes := Get_Boxes (Boxes_File_Name);
+
+   function Get_Distances (B : Junction_Boxes) return Box_Distances is
+      Index : Positive := 1;
+   begin
+      return D : Box_Distances (1 .. B'Last * (B'Last - 1) / 2) do
+         for I in B'First .. B'Last - 1 loop
+            for J in I + 1 .. B'Last loop
+               D (Index) := Pair_Distance'(A => I, B => J,
+                  Distance => Compute_Distance (B (I), B (J)));
+               Index := Index + 1;
+            end loop;
+         end loop;
+      end return;
+   end Get_Distances;
+
+   procedure Sort_Distance_Pairs is new
+      Ada.Containers.Generic_Array_Sort (
+         Index_Type => Positive,
+         Element_Type => Pair_Distance,
+         Array_Type => Box_Distances);
+
+   Distances : Box_Distances := Get_Distances (Boxes);
+
+   package Box_Sets is new
+      Ada.Containers.Bounded_Ordered_Sets (Element_Type => Positive);
+
+   subtype Box_Set is Box_Sets.Set
+      (Ada.Containers.Count_Type (Connected_Boxes));
+
+   subtype Closest is Positive range 1 .. Connections;
+   type Circuits is array (Closest) of Box_Set;
+
+   function "<" (Left, Right : Box_Set) return Boolean is
+      use Ada.Containers;
+   begin
+      return Left.Length < Right.Length;
+   end "<";
+
+   procedure Sort_Circuits is new
+      Ada.Containers.Generic_Constrained_Array_Sort (
+         Index_Type => Closest,
+         Element_Type => Box_Set,
+         Array_Type => Circuits);
+begin
+   Sort_Distance_Pairs (Distances);
+   Put_Line (Boxes'Image);
+   Put_Line (Distances'Image);
+end Day8;
